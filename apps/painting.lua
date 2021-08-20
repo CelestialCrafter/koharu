d0000a41e989a74b7e3dd7b1e9ce42534c067689
CelestialCrafter <celestialcraftermc@gmail.com>
2021-08-20 02:24:04 -0400

    read diffs

--- apps/painting.lua
+++ apps/painting.lua
@@ -1,1 +1,2 @@
-shell.run('paint ../appdata/' .. sha256.sha256(os.time() .. os.day()))
+-- I know its not a good way to store files but i dont feel like making reliable code rn
+shell.run('paint /appdata/' .. sha256.sha256(math.random(1, 100000000)))
