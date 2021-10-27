94c2d748c08062be54f448e9823600f38cbb1388
CelestialCrafter <celestialcraftermc@gmail.com>
2021-10-27 16:30:43 -0400

    gyiag

--- home.lua
+++ home.lua
@@ -1,17 +1,13 @@
 -- @TODO sendfile/command, receivefile/command, gps, 
-
-local y = 1
-local keepUi = true
-local appsLength = 8
 local apps = {
-	[1] = {'/apps/chatroom', 'Chatroom'},
-	[2] = {'/apps/painting', 'Painting'},
-	[3] = {'/apps/sendFile', 'SendFile'},
-	[4] = {'/apps/receiveFile', 'ReceiveFile'},
-	[5] = {'/apps/shutdown', 'Shutdown'},
-	[6] = {'/apps/reboot', 'Reboot'},
-	[7] = {'/apps/worm', 'Worm'},
-	[8] = {'/apps/createpassword', 'Create Password'}
+	{'/apps/chatroom', 'Chatroom'},
+	{'/apps/painting', 'Painting'},
+	{'/apps/sendFile', 'SendFile'},
+	{'/apps/receiveFile', 'ReceiveFile'},
+	{'/apps/shutdown', 'Shutdown'},
+	{'/apps/reboot', 'Reboot'},
+	{'/apps/worm', 'Worm'},
+	{'/apps/createpassword', 'Create Password'}
 }
 
-options.generateMenu(apps, function () shell.run(apps[y][1]) end)
+options.generateMenu(apps, function (appPath) shell.run(appPath) end)
--- libraries/options.lua
+++ libraries/options.lua
@@ -1,47 +1,48 @@
 -- @TODO sendfile/command, receivefile/command, gps, 
 
 function generateMenu(options, select, padding)
-local y = 1
-local keepUi = true
-padding = padding or {'-> ', ' <-'}
-options = {
-	[1] = {'option1', 'Option 1'},
-	[2] = {'option2', 'Option 2'},
-}
-optionLength = utils.getTableLength()
+	local y = 1
+	local keepUi = true
+	padding = padding or {'-> ', ' <-'}
+	options = {
+		{'option1', 'Option 1'},
+		{'option2', 'Option 2'},
+	}
+	optionLength = utils.getTableLength(options)
 
-while keepUi do
-	term.setBackgroundColor(colours.black)
-	utils.clear()
-	for i, option in ipairs(options) do
-    if (y == i) then
-			term.setBackgroundColor(colors.white)
-			term.setTextColor(colors.black)
-		end
-		print(padding[1] .. options[2] .. padding[2])
-    if (y == i) then
-			term.setBackgroundColor(colors.black)
-			term.setTextColor(colors.white)
-		end
-	end
-	
-	local event, key = os.pullEvent('key')
-
-	if key == keys.enter then
+	while keepUi do
+		term.setBackgroundColor(colours.black)
 		utils.clear()
-		keepUi = false
-		select(options[y][1])
-	elseif key == keys.up then
-		if y > 1 then
-			y = y - 1
-		elseif y == 1 then
-			y = appsLength
+		for i, option in ipairs(options) do
+			if (y == i) then
+				term.setBackgroundColor(colors.white)
+				term.setTextColor(colors.black)
+			end
+			print(padding[1] .. options[i][2] .. padding[2])
+			if (y == i) then
+				term.setBackgroundColor(colors.black)
+				term.setTextColor(colors.white)
+			end
 		end
-	elseif key == keys.down then
-		if y < appsLength then
-			y = y + 1
-		elseif y == appsLength then
-			y = 1
+		
+		local event, key = os.pullEvent('key')
+
+		if key == keys.enter then
+			utils.clear()
+			keepUi = false
+			select(options[y][1])
+		elseif key == keys.up then
+			if y > 1 then
+				y = y - 1
+			elseif y == 1 then
+				y = optionsLength
+			end
+		elseif key == keys.down then
+			if y < optionsLength then
+				y = y + 1
+			elseif y == optionsLength then
+				y = 1
+			end
 		end
 	end
 end
