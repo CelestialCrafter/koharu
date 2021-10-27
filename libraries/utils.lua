20e83d7d2226319d54c59611624671dac58515f0
CelestialCrafter <celestialcraftermc@gmail.com>
2021-10-27 00:21:21 -0400

    added getModem and scanForModem into utils

--- libraries/utils.lua
+++ libraries/utils.lua
@@ -1,3 +1,27 @@
+local sides = {"front", "back", "left", "right", "top", "bottom"}
+local detectedSide = nil
+local modem = nil
+local hasRednet = false
+
+function scanForModem()
+	for k, v in ipairs(sides) do
+		if peripheral.isPresent(v) then
+			if peripheral.getType(v) == "modem" then
+				detectedSide = v
+				hasRednet = true
+				modem = peripheral.wrap(v)
+				break
+			end
+		end
+	end
+end
+
+scanForModem()
+
+function getModem()
+	return modem
+end
+
 function clear()
 	term.clear()
 	term.setCursorPos(1, 1)
