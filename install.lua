6ed125b411bcc52f6a168c91367f705047b3088f
CelestialCrafter <celestialcraftermc@gmail.com>
2022-07-14 04:51:44 -0500

    added daidjkamdl

--- install.lua
+++ install.lua
@@ -388,7 +388,7 @@ end
 
 print('Are you sure you want to install? (THIS WILL WIPE YOUR DRIVE) [y/n]')
 local confirmation = io.read()
-local drive = ...
+local drive = ... or ''
 
 if (confirmation == 'y') then
 	for _, file in pairs(fs.list('/' .. drive)) do
