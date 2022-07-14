8817bfebf690124d962907d17bc25eabada2d462
CelestialCrafter <celestialcraftermc@gmail.com>
2022-07-14 03:12:14 -0500

    changed json library, updated install.lua

--- install.lua
+++ install.lua
@@ -1,11 +1,70 @@
-print("Are you sure you want to install? (THIS WILL WIPE YOUR DRIVE) [y/n]")
+local json = require('libraries.json')
+
+local function save(data,file)
+	local file = shell.resolve(file:gsub("%%20"," "))
+	if not (fs.exists(string.sub(file,1,#file - #fs.getName(file))) and fs.isDir(string.sub(file,1,#file - #fs.getName(file)))) then
+		if fs.exists(string.sub(file,1,#file - #fs.getName(file))) then fs.delete(string.sub(file,1,#file - #fs.getName(file))) end
+		fs.makeDir(string.sub(file,1,#file - #fs.getName(file)))
+	end
+	local f = fs.open(file,"w")
+	f.write(data)
+	f.close()
+end
+
+local function install(preset)
+	local data = json.decode(http.get('https://api.github.com/repos/' .. preset.user ..'/' .. preset.repo .. '/git/trees/' .. preset.branch .. '?recursive=1').readAll())
+	if data.message and data.message:find('API rate limit exceeded') then error('Out of API calls, try again later') end
+	if data.message and data.message == 'Not found' then error('Invalid repository', 2) else
+		for k, v in pairs(data.tree) do
+			if v.type == "tree" then
+				fs.makeDir(fs.combine(preset.path,v.path))
+			end
+		end
+		local filecount = 0
+		local downloaded = 0
+		local paths = {}
+		local failed = {}
+		for k, v in pairs(data.tree) do
+			-- Send all HTTP requests (async)
+			if v.type == "blob" then
+				v.path = v.path:gsub("%s","%%20")
+				local url = 'https://raw.github.com/' .. preset.user .. '/' .. preset.repo .. '/' .. preset.branch .. '/' .. v.path
+				http.request(url)
+				paths[url] = fs.combine(preset.path, v.path)
+				filecount = filecount + 1
+			end
+		end
+		while downloaded < filecount do
+			local e, a, b = os.pullEvent()
+			if e == 'http_success' then
+				save(b.readAll(), paths[a])
+				downloaded = downloaded + 1
+			elseif e == 'http_failure' then
+				-- Retry in 3 seconds
+				failed[os.startTimer(3)] = a
+			elseif e == 'timer' and failed[a] then
+				http.request(failed[a])
+			end
+		end
+	end
+end
+
+print('Are you sure you want to install? (THIS WILL WIPE YOUR DRIVE) [y/n]')
 local confirmation = io.read()
 local drive = ...
 
-if (confirmation == "y") then
-	shell.run("rm /" .. drive .. "/*")
-	shell.run("cp ./* /" .. drive)
-	print("Installation completed!")
+if (confirmation == 'y') then
+	for _, file in pairs(fs.list('/' .. drive)) do
+    fs.delete(file)
+	end
+
+	install({
+		user = 'CelestilaCrafter',
+		repo = 'bonfire',
+		branch = 'master',
+		path = drive
+	})
+	print('Installation completed!')
 else
-	print("Installation stopped.")
+	print('Installation stopped.')
 end
