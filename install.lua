d7a158b679fa5da10565d9ee80f9898b9aa0eb79
CelestialCrafter <celestialcraftermc@gmail.com>
2022-07-14 00:07:51 -0500

    updated install

--- install.lua
+++ install.lua
@@ -1,9 +1,10 @@
 print("Are you sure you want to install? (THIS WILL WIPE YOUR DRIVE) [y/n]")
 local confirmation = io.read()
+local drive = ...
 
 if (confirmation == "y") then
-	shell.run("rm /" .. arg[1] .. "/*")
-	shell.run("mv ./* /" .. arg[1])
+	shell.run("rm /" .. drive .. "/*")
+	shell.run("mv ./* /" .. drive)
 	print("Installation completed!")
 else
 	print("Installation stopped.")
