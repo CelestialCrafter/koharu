93f92a09ed0d68c6ca1a376dade88a262aa22573
CelestialCrafter <celestialcraftermc@gmail.com>
2022-08-03 19:45:33 -0500

    whoops, forgot to parse the id array

--- apps/sendCommand.lua
+++ apps/sendCommand.lua
@@ -1,11 +1,12 @@
 local utils = require('/libraries/utils')
 
 print('What device ID would you like to commands to?')
-local id = tonumber(utils.split(read(), ' '))
-
-print('What command would you like to send?')
-while (true) do
-	write('$ ')
-	local command = read()
-	rednet.send(id, command, 'ssh')
+local stringIds = utils.split(read(), ' ')
+for id in tonumber(stringIds) do
+	print('What command would you like to send?')
+	while (true) do
+		write('$ ')
+		local command = read()
+		rednet.send(id, command, 'ssh')
+	end
 end
