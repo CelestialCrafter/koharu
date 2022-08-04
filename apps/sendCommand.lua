5e2b4d6164ab3a91520266f4d1d96acde958d1e8
CelestialCrafter <celestialcraftermc@gmail.com>
2022-08-03 19:36:40 -0500

    added split to utils added the ability to send files to multiple machines

--- apps/sendCommand.lua
+++ apps/sendCommand.lua
@@ -1,5 +1,7 @@
+local utils = require('/libraries/utils')
+
 print('What device ID would you like to commands to?')
-local id = tonumber(read())
+local id = tonumber(utils.split(read(), ' '))
 
 print('What command would you like to send?')
 while (true) do
--- libraries/utils.lua
+++ libraries/utils.lua
@@ -64,6 +64,17 @@ function utils.filterTable(table, filterFunction)
 	return output
 end
 
+function utils.split(inputstr, sep)
+	if sep == nil then
+		sep = "%s"
+	end
+	local t = {}
+	for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
+		table.insert(t, str)
+	end
+	return t
+end
+
 utils.scanForModem()
 utils.openRednet()
 
